63ca5927e390d088791c47a84bc21f5df32108f5
tryiou <73646876+tryiou@users.noreply.github.com>
2023-11-10 01:03:14 +0100

    Update config_pingpong.py

--- config/config_pingpong.py
+++ config/config_pingpong.py
@@ -5,31 +5,41 @@
 # each line is one side only
 
 debug_level = 2  # 0=Off, 2=Display RPC calls, 3=Display answers
+
 user_pairs = [
-    # "DOGE/LTC",
-    # "LTC/DOGE",
-    # "LTC/PIVX",
-    # "PIVX/LTC",
     "BLOCK/LTC",
-    "LTC/BLOCK",
-    # "RVN/LTC",
-    # "LTC/RVN",
-    "LTC/DASH",
-    "DASH/LTC",
+    "LTC/BLOCK"
+    # "LTC/DASH",
+    # "DASH/LTC",
     # "BLOCK/DASH",
     # "DASH/BLOCK",
-    # "BLOCK/PIVX",
-    # "PIVX/BLOCK",
+    # "DOGE/LTC",
+    # "LTC/DOGE"
 ]
 
-cc_coins = []
-# "BTC"
-# "DASH",
+# price_variation_tolerance = CANCEL AND REFRESH ORDER IF PRICE CHANGE MORE THAN THIS % SET
+price_variation_tolerance = 0.01  # 0.01=1%
 
 # sell_price_offset = % upscale applied to xb sell price based on ccxt ticker
 sell_price_offset = 0.05
 
-# spread = min % lock on xb rebuy action:
+# usd_amount = PER ORDER USD_TO_TOKEN1 AMOUNT ("BLOCK/LTC" = "token1/token2")
+# gather token1/2/usd prices tickers to calc orders sizes
+# custom to use per pair setting, else use default
+usd_amount_default = 5  # 5 $USD PER ORDER
+# optional >
+usd_amount_custom = {
+    # "DASH/BLOCK": 30,
+    # "BLOCK/DASH": 30,
+    # "DASH/LTC": 30
+    # "BLOCK/LTC": 500,
+    # "BLOCK/BTC": 50,
+    # "BLOCK/LTC": 50,
+    # "LTC/BLOCK": 50
+}
+# optional<
+
+# spread = min % profit lock on PONG action:
 # (cex_price *(1+sell_price_offset)) to sell, (sold_price*(1-spread)) to rebuy
 # custom to use per pair setting, else use default
 spread_default = 0.05  # 0.05=5%
@@ -47,28 +57,13 @@ spread_custom = {
     # "BLOCK/PIVX": 0.04
     # "BLOCK/BTC": 0.1
 }
-# usd_amount = PER ORDER USD_TO_TOKEN1 AMOUNT ("BLOCK/LTC" = "token1/token2")
-# convert token price to usd to calc order size
-# custom to use per pair setting, else use default
-usd_amount_default = 5  # 5 $USD PER ORDER
-# optional >
-usd_amount_custom = {
-    # "DASH/BLOCK": 30,
-    # "BLOCK/DASH": 30,
-    # "DASH/LTC": 30
-    # "BLOCK/LTC": 500,
-    # "BLOCK/BTC": 50,
-    # "BLOCK/LTC": 50,
-    # "LTC/BLOCK": 50
-}
-# price_variation_tolerance = CANCEL AND REFRESH ORDER IF PRICE CHANGE MORE THAN THIS % SET
-price_variation_tolerance = 0.01  # 0.01=1%
+# optional <
 
+# obsolete >
 # display_max_stars = CHANGE RETURN TO LINE AFTER PRINTING STARS DURING ORDER CHECKS, ONLY CHANGE CONSOLE DISPLAY
 display_max_stars = 10
-
-# optional func>
+cc_coins = [] # "BTC" # "DASH",
 arb_team = False
 arb_team_spread = 0.04
 arb_team_pairs = ["BLOCK/BTC"]  # ["BLOCK/LTC"]
-# optional<
+# obsolete <
