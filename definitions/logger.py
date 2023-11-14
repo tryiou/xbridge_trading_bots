65294b4c6f82976dec51c7971c003152f06401ab
tryiou <whzfck@gmail.com>
2023-11-14 21:05:14 +0100

    optional 'logfile'

--- definitions/logger.py
+++ definitions/logger.py
@@ -3,16 +3,22 @@ import logging
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(message)s')  # '%(asctime)s %(levelname)s %(message)s')
 
 
-def setup_logger(name, log_file, level=logging.INFO, console=False):
-    """To setup as many loggers as you want"""
-    handler = logging.FileHandler(log_file)
-    handler.setFormatter(formatter)
-    # handler.setFormatter()
+def setup_logger(name, log_file=None, level=logging.INFO, console=False):
+    """To set up as many loggers as you want"""
     log_handle = logging.getLogger(name)
     log_handle.setLevel(level)
-    log_handle.addHandler(handler)
+
+    # Add FileHandler if log_file is provided
+    if log_file:
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        handler.setLevel(level)  # Set handler level to match logger level
+        log_handle.addHandler(handler)
+
+    # Add StreamHandler for console logging
     if console:
         ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        ch.setLevel(level)  # Set console handler level to match logger level
         log_handle.addHandler(ch)
+
     return log_handle
