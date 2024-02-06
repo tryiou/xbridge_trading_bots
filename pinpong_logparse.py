10a40b3a6a21fb9dd3ba4d317a6566f363a5e071
tryiou <whzfck@gmail.com>
2024-02-06 22:36:47 +0100

    add a "--timeframe" arg to pingpong_logparse

--- pinpong_logparse.py
+++ pinpong_logparse.py
@@ -1,7 +1,8 @@
 import re
 import json
 from tabulate import tabulate
-from datetime import datetime
+from datetime import datetime, timedelta
+import argparse
 
 
 def reverse_side(s):
@@ -11,8 +12,9 @@ def reverse_side(s):
         return "BUY"
 
 
-def read_log_file(log_file_path):
+def read_log_file(log_file_path, timeframe=None):
     result = {}
+    now = datetime.now()
 
     with open(log_file_path, 'r') as file:
         log_data = file.read()
@@ -30,6 +32,10 @@ def read_log_file(log_file_path):
                 myjson = json.loads(json_part)
 
                 if myjson:
+                    trade_datetime = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S,%f")
+                    if timeframe is not None and now - trade_datetime >= timeframe:
+                        continue
+
                     if not myjson['symbol'] in result:
                         result[myjson['symbol']] = []
                     maker = myjson['maker']
@@ -37,14 +43,12 @@ def read_log_file(log_file_path):
                     taker = myjson['taker']
                     taker_size = float(f"{myjson['taker_size']:.6f}")
                     if myjson['side'] == "BUY":
-                        # reverse maker/taker for reading logic
                         buff = maker
                         buff_size = maker_size
                         maker = taker
                         maker_size = taker_size
                         taker = buff
                         taker_size = buff_size
-                    # store result
                     result[myjson['symbol']].append([
                         timestamp,
                         myjson['side'],
@@ -71,35 +75,33 @@ def calculate_profit(trades):
             timestamp, side, maker_size, maker, reverse_side, taker_size, taker = trade
 
             if side == 'SELL':
-                # Update 'SELL' side asset count
                 if taker not in asset_count_sell:
                     asset_count_sell[taker] = 0.0
                 asset_count_sell[taker] += taker_size
 
-                # Keep track of the last 'SELL' trade
                 last_sell_trade = trade
 
             elif side == 'BUY':
-                # Update 'BUY' side asset count
                 if maker not in asset_count_buy:
                     asset_count_buy[maker] = 0.0
                 asset_count_buy[maker] += maker_size
-                # Check if there was a previous 'SELL' trade
+
                 if last_sell_trade:
-                    _, _, _, _, _, last_taker_size, last_taker = last_sell_trade
-                    # Calculate profit using the last 'SELL' and current 'BUY' trades
-                    profit = float(f"{last_taker_size - taker_size:.6f}")
-                    if symbol not in completed_pingpong:
-                        completed_pingpong[symbol] = []
-                    completed_pingpong[symbol].append([last_sell_trade, trade, f"{profit} {taker}"])
-                    if symbol not in profit_pingpong:
-                        profit_pingpong[symbol] = {'profit': profit, 'asset': taker}
-                    else:
-                        profit_pingpong[symbol]['profit'] += profit
-                        profit_pingpong[symbol]['profit'] = float(f"{profit_pingpong[symbol]['profit']:.6f}")
+                    _, _, last_maker_size, last_maker, _, last_taker_size, last_taker = last_sell_trade
+                    # print(last_maker_size, last_maker, last_taker_size, last_taker)
+                    # print(maker_size, maker, taker_size, taker)
+                    if last_maker_size == maker_size:
+                        profit = float(f"{last_taker_size - taker_size:.6f}")
+                        if symbol not in completed_pingpong:
+                            completed_pingpong[symbol] = []
+                        completed_pingpong[symbol].append([last_sell_trade, trade, f"{profit} {taker}"])
+                        if symbol not in profit_pingpong:
+                            profit_pingpong[symbol] = {'profit': profit, 'asset': taker}
+                        else:
+                            profit_pingpong[symbol]['profit'] += profit
+                            profit_pingpong[symbol]['profit'] = float(f"{profit_pingpong[symbol]['profit']:.6f}")
                     last_sell_trade = None
 
-        # Check if there was a 'SELL' without a subsequent 'BUY'
         if last_sell_trade:
             if symbol not in inprogress_pingpong:
                 inprogress_pingpong[symbol] = []
@@ -107,33 +109,26 @@ def calculate_profit(trades):
 
     return completed_pingpong, inprogress_pingpong, profit_pingpong
 
-# Define a custom sorting key function
+
 def custom_sort(item):
-    return (item[0], item[1])  # Assuming the first item is the symbol and the second is the timestamp
+    return (item[0], item[1])
+
 
 def display_results(completed_pingpong, inprogress_pingpong, profit_pingpong):
-    # Convert the dictionary to a list of tuples for tabulation with trades flattened
     completed_table_data = []
 
     for symbol, trades_list in completed_pingpong.items():
         for pingpongsequence in trades_list:
             for event in pingpongsequence:
-                # 1PING #2PONG #3PROFIT
                 if isinstance(event, list):
                     timestamp, side, maker_size, maker, r_side, taker_size, taker = event
                     if side == "BUY":
-                        # Example dates
                         date_str1 = pingpongsequence[0][0]
                         date_str2 = pingpongsequence[1][0]
-
-                        # Convert string representations to datetime objects
                         date_format = "%Y-%m-%d %H:%M:%S,%f"
                         date1 = datetime.strptime(date_str1, date_format)
                         date2 = datetime.strptime(date_str2, date_format)
-
                         delta_date = str(date2 - date1).split('.')[0]
-
-                        # Assuming profit is the third element in the pingpongsequence
                         profit = pingpongsequence[2]
                     else:
                         delta_date = ""
@@ -142,39 +137,57 @@ def display_results(completed_pingpong, inprogress_pingpong, profit_pingpong):
                     completed_table_data.append(
                         (symbol, timestamp, side, maker_size, maker, r_side, taker_size, taker, profit, delta_date))
 
-    # Ensure the headers match the number of columns in the completed_table_data
     headers = ["Symbol", "Timestamp", "Side", "Size T1", "Token1", "R_Side", "Size T2", "Token2", "Profit",
                "Exec time (D, h:m:s)"]
 
-    # Print the tabulated data with left-aligned content
     print(tabulate(completed_table_data, headers=headers, tablefmt="pretty"))
 
     print("\nInprogress Pingpong:")
-    # Convert the dictionary to a list of tuples for tabulation with trades flattened
     inprogress_table_data = [(symbol, *trade) for symbol, trades in inprogress_pingpong.items() for trade in trades]
     sorted_inprogress = sorted(inprogress_table_data, key=custom_sort)
-    # Print the tabulated data
     print(tabulate(sorted_inprogress,
                    headers=["Symbol", "Timestamp", "Side", "Size T1", "Token1", "R_Side", "Size T2",
                             "Token2"],
                    tablefmt="pretty"))
 
     print("\nProfit Pingpong:")
-    # Convert the dictionary to a list of tuples for tabulation with profit values flattened
     profit_table_data = [(symbol, profit['profit'], profit['asset']) for symbol, profit in profit_pingpong.items()]
-
-    # Print the tabulated data
     print(tabulate(profit_table_data, headers=["Symbol", "Profit", "Asset"], tablefmt="pretty"))
 
 
+def parse_timeframe(timeframe_str):
+    if timeframe_str.lower() == 'all':
+        print("Timeframe: All")
+        return None
+
+    time_units = {
+        'day': timedelta(days=1),
+        'days': timedelta(days=1),
+        'month': timedelta(days=30),
+        'months': timedelta(days=30),
+        'year': timedelta(days=365),
+        'years': timedelta(days=365)
+    }
+
+    for unit, value in time_units.items():
+        if unit in timeframe_str:
+            num_units = int(timeframe_str.split(unit)[0])
+            parsed_result = value * num_units
+            print(f"Timeframe: {num_units} {unit}(s)")
+            return parsed_result
+
+    print("Invalid timeframe format. Supported formats: day(s), month(s), year(s)")
+    return None
+
+
 def main():
-    # Specify the path to your log file
+    parser = argparse.ArgumentParser(description='Pingpong Log Parser')
+    parser.add_argument('--timeframe', type=str, default='all', help='Specify the timeframe to parse from now date (e.g., "3months", "4days", "2years")')
+    args = parser.parse_args()
+
     log_file_path = "logs/pingpong_trade.log"
-
-    trades = read_log_file(log_file_path)
+    trades = read_log_file(log_file_path, parse_timeframe(args.timeframe))
     completed_pingpong, inprogress_pingpong, profit_pingpong = calculate_profit(trades)
-
-    # Display your results or use them as needed
     display_results(completed_pingpong, inprogress_pingpong, profit_pingpong)
 
 
