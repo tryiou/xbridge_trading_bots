14830a2886f573ea91c903683c370d7d61f806fe
tryiou <tryou@tryou.com>
2025-05-19 11:01:51 +0200

    refactor: add flush capability to console logging handler

--- definitions/logger.py
+++ definitions/logger.py
@@ -4,32 +4,34 @@ import os
 formatter = logging.Formatter('[%(asctime)s] [%(module)s] %(levelname)s - %(message)s')
 
 
-def setup_logging(name, log_file=None, level=logging.INFO, console=False):
-    """To set up as many loggers as you want"""
-    # Define formatter
+class FlushStreamHandler(logging.StreamHandler):
+    def emit(self, record):
+        super().emit(record)
+        self.flush()
 
+
+def setup_logging(name, log_file=None, level=logging.INFO, console=False):
+    """To set up as many loggers as you want, with console flushing"""
     log_handle = logging.getLogger(name)
     log_handle.setLevel(level)
 
-    # Clear existing handlers to avoid duplicates if called multiple times
     if log_handle.handlers:
         log_handle.handlers.clear()
 
-    # Add FileHandler if log_file is provided
     if log_file:
         handler = logging.FileHandler(log_file)
         handler.setFormatter(formatter)
         handler.setLevel(level)
         log_handle.addHandler(handler)
 
-    # Add StreamHandler for console logging
     if console:
-        ch = logging.StreamHandler()
-        ch.setFormatter(formatter)  # Also apply formatter to console handler
+        ch = FlushStreamHandler()
+        ch.setFormatter(formatter)
         ch.setLevel(level)
         log_handle.addHandler(ch)
 
     return log_handle
+
 
 
 def setup_logger(strategy=None):
